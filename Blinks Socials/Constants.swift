adc04f9e4c17cd065ccde45a65d93c439fe3ba79
MishariSH <misharish@MishariSHs-Mac.local>
2016-11-13 12:24:34 -0800

    Sign in UI Custom classes complete

---
+++ Blinks Socials/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Blinks Socials
+//
+//  Created by MishariSH on 11/13/16.
+//  Copyright © 2016 MishariSH. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
